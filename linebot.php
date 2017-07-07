6f67aa5ea71c8b9410e9ded1c1e841daa0f91cfb
soranakiz <soranakiz@gmail.com>
2017-07-07 14:56:56 +0700

    Update linebot.php

--- linebot.php
+++ linebot.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'izEK3fPoNJsOUJW2eQ0v3En4AZJNcq4oqE/PKoBH45ZPiicl2BiASrdgoYQ0RtfZZhehS5tCWQSCZmnk2Cp/D8lef0BznJLA4aAU8vgOfeenN70Wegu5P7/C8iX1AEnZvb7VW+GSGRMGM1uhdrjGHgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v1/oauth/linebot';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
