764ccd4ee0877273302826d17c7ab2919dce2e7e
soranakiz <soranakiz@gmail.com>
2017-07-07 17:30:20 +0700

    Update getapi.php

--- getapi.php
+++ getapi.php
@@ -15,5 +15,45 @@ $ch = curl_init();
         echo "ความเร็วในการขุดปัจจุบัน : ".$DATA['hashRate']."<br>";
         echo "ความเร็วในการขุดเฉลี่ย : ".$DATA['avgHashrate']."H/s<br>";
         echo "ZEC ในบัญชี : ".$DATA['unpaid']."<br>";
-        echo "ทดสอบตัวแปร workers".$DATA['name']."<br>";
+        echo "หลัง""<br>";
+
+$access_token = 'izEK3fPoNJsOUJW2eQ0v3En4AZJNcq4oqE/PKoBH45ZPiicl2BiASrdgoYQ0RtfZZhehS5tCWQSCZmnk2Cp/D8lef0BznJLA4aAU8vgOfeenN70Wegu5P7/C8iX1AEnZvb7VW+GSGRMGM1uhdrjGHgdB04t89/1O/w1cDnyilFU=';
+if (!is_null($DATA['DATA'])) {
+	// Loop through each event
+	foreach ($DATA['DATA'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
         ?>
