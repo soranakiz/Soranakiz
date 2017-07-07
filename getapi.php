c5b9d898b639141d668b8fda2389f5c3ef50d970
soranakiz <soranakiz@gmail.com>
2017-07-07 15:38:08 +0700

    Create getapi.php

---
+++ getapi.php
@@ -0,0 +1,14 @@
+<?php
+$ch = curl_init(); 
+        // set url สำหรับดึงข้อมูล 
+        curl_setopt($ch, CURLOPT_URL, "http://zcash.flypool.org/api/miner_new/t1csgRshvziESnqzp68LJbANGy2JRm86xJp/?type=json"); 
+        //return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        // ตัวแปร $output เก็บข้อมูลทั้งหมดที่ดึงมา 
+        $output = curl_exec($ch); 
+      
+    	// ปิดการเชื่อต่อ
+        curl_close($ch);    
+        // output ออกไปครับ
+        echo $output;
+        ?>
